b93950620a92cfc671c516aee37299ebc50c9c4a
ikerlopezmr358-IA <ikerlopezmr358@gmail.com>
2026-02-12 14:23:25 -0700

    Enhance calculator to accept user inputs
    Updated the calculator to allow user input for price and amount.

--- Dia2.py
+++ Dia2.py
@@ -1,6 +1,6 @@
-# Calculadora_simple
-Mi primera calculadora simple :)
-price=10
-amount=10
+# calculadora simple version 2, la e mejorado un poco danle al usuario la capacidad de poner la cantidad y el numero que desea multiplicar.
+price= int(input("introduce la cantidad que deseas multiplicar: "))
+amount=int(input("con que numero deseas multiplicarlo?: "))
 total=price*amount
 print(total)
+
